b03026af865ab14c81bff6d04675a577469604ee
stark <mrcs.stark@gmail.com>
2020-08-30 21:08:35 -0300

    docs: Add mais teste para repositories e services

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,14 +4,14 @@ import service.impl.TransactionService
 import utils.Constants
 import utils.ExtractPrinter
 import utils.TransactionPrinter
+import java.util.*
 
 fun main() {
     println("Challenge Blu by Bs2")
 
-//    val scanner = Scanner(System.`in`)
-//    println("Insira o caminho do arquivo de transações")
-//    val pathFile: String = scanner.nextLine()
-    val pathFile = "/home/marcos/Documentos/transactions.csv"
+    val scanner = Scanner(System.`in`)
+    println("Insira o caminho do arquivo de transações")
+    val pathFile: String = scanner.nextLine()
 
     //Caso queira carregar o arquivo diretamente da pasta resources
     //val pathFile = Utils.loadResource("transactions.csv")
@@ -34,16 +34,12 @@ fun main() {
     // Executar as operações de deposito
     val extract = transactionService.processTransaction(transactionsList)
 
-    println("----------------------------------------------------------------")
     if(extract.isNotEmpty()) {
+        println("----------------------------------------------------------------------")
         println("Arquivo de extrato salvo em: ${Constants.PATH_RESOURCES}/${Constants.EXTRACT_FILENAME}")
     }
-    // Ler aquivo de extrato e visualizar
+
     val extractPrinter: ExtractPrinter
-//    val extractPathFile = Utils.loadFilesResource("extract.csv")
-//    val extractRepository = ExtractFileRepository(extractPathFile)
-//    val extractService = ExtractService(extractRepository)
-//    val extractList = extractService.findAll()
     extractPrinter = ExtractPrinter(extract)
     extractPrinter.print()
 }
